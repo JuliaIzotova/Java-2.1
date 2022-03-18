dfbcdcd4411c5d623cde25f18a7f949cceea5d54
JuliaIzotova] <yuizotova15@gmail.com>
2022-03-18 12:05:43 +0300

    init commit3

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,10 @@ public class Main<milesEarned> {
         int ticketPrice = 1_000;
         int milePrice = 20;
 
-        // Решение
-
+        // Решениу
+        int milesEraned;
         int milesEarned = (int) ticketPrice / milePrice;
 
-        int milesEarned1 = milesEarned;
         System.out.println("Накопленные мили:");
         System.out.println(milesEarned);
     }
